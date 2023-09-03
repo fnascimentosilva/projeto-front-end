119a9fb934dbc86ff2f7328ce1a9806ad79ce198
fnascimentosilva <fabricionsilva26@gmail.com>
2023-09-03 19:05:24 +0100

    inserindo um app-bar fixo para as telas

--- nerd-gym/src/App.vue
+++ nerd-gym/src/App.vue
@@ -1,6 +1,37 @@
 <template>
- <h1>Iniciando o projeto</h1>
- <router-view></router-view>
+  <v-layout>
+    <v-app-bar color="grey-lighten-3" v-if="renderizarMenu">
+      <v-app-bar-title class="titleMenu">Nerd Gym</v-app-bar-title>
+
+      <v-btn>
+        <router-link to="/dashboard"> Home </router-link>
+      </v-btn>
+      <v-btn>
+        <router-link to="/"> Alunos </router-link>
+      </v-btn>
+      <v-btn>
+        <router-link to="/"> Exercícios </router-link>
+      </v-btn>
+      <v-btn>
+        <router-link to="/"> Sair </router-link>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-layout>
+  
 </template>
+
+<script>
+export default {
+  computed: {
+    renderizarMenu() {
+      return this.$route.path !== '/' && this.$route.path !== '/cadastro'
+    }
+  }
+}
+</script>
 
 
