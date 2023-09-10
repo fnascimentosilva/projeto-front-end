1d0d4b198d34fad4b026d81445ea661c5a0b6ced
fnascimentosilva <fabricionsilva26@gmail.com>
2023-09-10 20:52:14 +0100

    finalizando tela cadastro de exercicio

--- nerd-gym/src/views/Exercicios/GerenciamentoExercicios.vue
+++ nerd-gym/src/views/Exercicios/GerenciamentoExercicios.vue
@@ -1,105 +1,105 @@
 <template>
-     <v-card class="mx-auto" :width="720">
-    <div class="d-flex align-center flex-wrap">
+  <v-card class="mx-auto" :width="720">
+    <div class="d-flex align-center">
       <v-icon>mdi-account-multiple</v-icon>
       <h1>Exercícios</h1>
     </div>
-    <hr cols="12" />
+    <hr />
     <v-form ref="form" @submit.prevent="handleCreateExercise" variant="outlined">
       <v-container>
-        <v-row>
-          <v-col cols="8" md="4">
+        <v-row class="d-flex align-center justify-center">
+          <v-col cols="8" md="">
             <v-text-field
               v-model="nomeExercicio"
-              label="Nome Completo"
+              label="Digite o nome do exercicio"
               :rules="nomeExercicioRules"
               required
             ></v-text-field>
           </v-col>
 
-         <v-btn  type="submit" block class="mt-2" variant="tonal" color="purple">Cadastrar</v-btn>
-
-        </v-row>
-
-      </v-container>
-
-      
-
-      <v-container>
-        <v-row>
-          <v-col cols="3" md="4">
-            
-            <v-text-field
-            
-              
-            > {{nomeExercicio}}</v-text-field>
+          <v-col cols="2">
+            <v-btn type="submit" block class="mt-2" variant="tonal" color="purple">Cadastrar</v-btn>
           </v-col>
-
-          </v-row>
+        </v-row>
       </v-container>
-      
     </v-form>
+    <v-table>
+      <t-head>
+        <th>Nome</th>
+      </t-head>
+      <tbody>
+        <tr v-for="exercicio in exercicios" :key="exercicio.id" width="400">
+          <td>{{ exercicio.description }}</td>
+        </tr>
+      </tbody>
+    </v-table>
   </v-card>
 </template>
 
 <script>
-
-
 import axios from 'axios'
-
 
 export default {
   data() {
     return {
-      nomeExercicio:[]
-      
+      nomeExercicio: '',
+      exercicios: [],
     }
   },
   mounted() {
-    
+    this.loadExercicios()
   },
 
-    handleCreateAccount() {
-      
-       if(this.$refs.form.validate()){
-        
-        axios({
-          url: 'http://localhost:3000/exercises',
-          method: 'POST',
-          data: {
-            description: this.nomeExercicio,
-            
-          }
-        })
-          .then(() => {
-            alert('Cadastrado com sucesso')
-            this.$router.push('/dashboard')
-          })
-          .catch((error) => {
-            console.log(error)
-            if (error.response?.data?.message) {
-              alert(error.response.data.message)
-            } else {
-              alert('Não foi possível criar a conta nesse momento')
-            }
-          })
-   }
- 
+  methods: {
+    async handleCreateExercise() {
+     const { valid } = await this.$refs.form.validate()
+
+    if (valid) {
+    axios({
+      url: 'http://localhost:3000/exercises',
+      method: 'POST',
+      data: {
+        description: this.nomeExercicio
+      }
+    })
+      .then(() => {
+        alert('Cadastrado com sucesso')
+        this.$router.push('/gerenciamento_exercicio')
+      })
+      .catch((error) => {
+        console.log(error)
+        /* if (error.response?.data?.message) {
+            alert(error.response.data.message)
+          } else {
+            alert('Não foi possível cadastrar o exercício')
+          } */
+      })
+     } else {
+      alert('Preencha os campos obrigatórios!')
+    }
   },
+
+    loadExercicios() {
+      axios({
+        url: 'http://localhost:3000/exercises',
+        method: 'GET'
+      })
+        .then((response) => {
+          this.exercicios = response.data
+        })
+        .catch(() => {
+          alert('Não há exerícios cadastrados')
+        })
+    }
+  },
+
+ 
   computed: {
     nomeExercicioRules() {
-      return [
-        (v) => !!v || 'Campo é obrigatório',
-      ];
-    },
-    
-  },
-
+      return [(v) => !!v || 'Campo é obrigatório']
+    }
+  }
 }
-
-
-
-
 </script>
 
 <style scoped>
