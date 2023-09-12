7da59065b81ccc6a2f12da67e1eb65fafe03c38d
fnascimentosilva <fabricionsilva26@gmail.com>
2023-09-12 11:10:52 +0100

    configurando o select dias da semana

--- nerd-gym/src/views/Exercicios/MontagemTreino.vue
+++ nerd-gym/src/views/Exercicios/MontagemTreino.vue
@@ -1,9 +1,99 @@
 <template>
-    <h1>Cadastro treino</h1>
+  <v-card class="mx-auto" :width="720">
+    <div class="d-flex align-center">
+      <v-icon>mdi-arm-flex</v-icon>
+      <h1>Treino</h1>
+    </div>
+    <v-form ref="form" @submit="handleCreateTreino" variant="outlined">
+      <v-container>
+        <v-row>
+          <v-col cols="6" md="4">
+            <v-select
+              v-model="diaSelecionado"
+              :items="diaDaSemana"
+              label="Selecione um dia da semana"
+              :rules="diaDaSemanaRules"
+            ></v-select>
+            
+          </v-col>
+
+          <v-col cols="6" md="4">
+            <v-select
+              v-model="exercicio"
+              label="Escolha o exercício"
+              :rules="exercicioRules"
+            ></v-select>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col cols="4" md="4">
+            <v-text-field
+              v-model="repetições"
+              label="Repetições"
+              :rules="repetiçõesRules"
+              required
+            ></v-text-field>
+          </v-col>
+
+          <v-col cols="4" md="4">
+            <v-text-field v-model="peso" label="Peso" :rules="pesoRules" required></v-text-field>
+          </v-col>
+          <v-col cols="4" md="4">
+            <v-text-field
+              v-model="descanso"
+              label="Descanso"
+              :rules="descansoRules"
+              required
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" md="4">
+            <v-textarea
+              v-model="descanso"
+              label="Observações"
+              :rules="descansoRules"
+              required
+            ></v-textarea>
+          </v-col>
+        </v-row>
+
+        
+      </v-container>
+      
+
+      <v-card-actions class="d-flex justify-end">
+     
+        <v-btn class="mt-2" variant="tonal" color="purple">Cancelar</v-btn>
+     <v-btn type="submit" class="mt-2" variant="tonal" color="purple">Cadastrar</v-btn>
+     
+     </v-card-actions>
+    </v-form>
+  </v-card>
 </template>
 
 <script>
+
+import axios from 'axios'
+
 export default {
-    
+    data() {
+        return {
+            diaDaSemana: ["segunda-feira", 'terça-feira', 'quarta-feira', 'quinta-feira', 'sexta-feira', 'sábado', 'domingo'],
+            diaSelecionado: "",
+        }
+    },
+mounted() {
+    this.carregarExercicios(
+    )
+},
+
+methods: {
+    carregarExercicios(){
+        
+    }
+},
+
 }
 </script>
