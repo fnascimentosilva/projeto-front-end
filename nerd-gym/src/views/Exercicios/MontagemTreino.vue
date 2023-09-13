b7c0f8310392820bd969546331ce21c03b94599b
fnascimentosilva <fabricionsilva26@gmail.com>
2023-09-13 10:35:30 +0100

    configuracao select exercicio

--- nerd-gym/src/views/Exercicios/MontagemTreino.vue
+++ nerd-gym/src/views/Exercicios/MontagemTreino.vue
@@ -5,7 +5,6 @@
       <h1>Treino</h1>
     </div>
 
-    
     <v-form ref="form" @submit="handleCreateTreino" variant="outlined">
       <v-container>
         <v-row>
@@ -16,19 +15,17 @@
               label="Selecione um dia da semana"
               :rules="diaDaSemanaRules"
             ></v-select>
-            
           </v-col>
 
           <v-col cols="6" md="4">
-           
             <v-select
-              v-model=" exercicioSelecionado"
-            
+              v-model="exercicioSelecionado"
+              item-title="description"
+              item-value="id"
               :items="exercicios"
               label="Escolha o exercício"
               :rules="exercicioRules"
             ></v-select>
-           
           </v-col>
         </v-row>
 
@@ -56,52 +53,49 @@
         </v-row>
         <v-row>
           <v-col cols="12" md="4">
-            <v-textarea
-              v-model="observacoes"
-              label="Observações"
-              
-            ></v-textarea>
+            <v-textarea v-model="observacoes" label="Observações"></v-textarea>
           </v-col>
         </v-row>
-
-        
       </v-container>
-      
 
       <v-card-actions class="d-flex justify-end">
-     
         <v-btn class="mt-2" variant="tonal" color="purple">Cancelar</v-btn>
-     <v-btn type="submit" class="mt-2" variant="tonal" color="purple">Cadastrar</v-btn>
-     
-     </v-card-actions>
+        <v-btn type="submit" class="mt-2" variant="tonal" color="purple">Cadastrar</v-btn>
+      </v-card-actions>
     </v-form>
   </v-card>
 </template>
 
 <script>
-
 import axios from 'axios'
 
 export default {
-    data() {
-        return {
-            diaDaSemana: ["segunda-feira", 'terça-feira', 'quarta-feira', 'quinta-feira', 'sexta-feira', 'sábado', 'domingo'],
-            diaSelecionado: "",
-            exercicios:[{}],
-            exercicioSelecionado:"",
-        }
-    },
-mounted() {
-    this.carregarExercicios(
-    )
-},
+  data() {
+    return {
+      diaDaSemana: [
+        'segunda-feira',
+        'terça-feira',
+        'quarta-feira',
+        'quinta-feira',
+        'sexta-feira',
+        'sábado',
+        'domingo'
+      ],
+      diaSelecionado: '',
+      exercicios: [{}],
+      exercicioSelecionado: ''
+    }
+  },
+  mounted() {
+    this.carregarExercicios()
+  },
 
-methods: {
-    carregarExercicios(){
-       axios({
-         url: 'http://localhost:3000/exercises',
+  methods: {
+    carregarExercicios() {
+      axios({
+        url: 'http://localhost:3000/exercises',
         method: 'GET'
-       }) 
+      })
         .then((response) => {
           this.exercicios = response.data
         })
@@ -109,7 +103,6 @@ methods: {
           alert('Exercícios não podem ser carregados')
         })
     }
-},
-
+  }
 }
 </script>
