49bfb01287a402f9b6c7285bba93766b23d0a92c
fnascimentosilva <fabricionsilva26@gmail.com>
2023-09-15 11:53:18 +0100

    criando tela ver treino

---
+++ nerd-gym/src/views/Exercicios/VerTreinoAluno.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Treino Aluno</h1>
+    {{student_name}}
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            student_name: localStorage.getItem('student_name');
+        }
+    },
+}
+</script>
